18c3f7033b9d9c4264f4d3864496b1f75f739abe
mark146 <tkdtkdqja@naver.com>
2018-07-01 19:36:22 +0900

    Spring MVC Controller Test

--- exam_1/src/main/java/org/com/controller/HomeController.java
+++ exam_1/src/main/java/org/com/controller/HomeController.java
@@ -4,12 +4,16 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import org.com.vo.ProductVO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * Handles requests for the application home page.
@@ -36,4 +40,50 @@ public class HomeController {
 		return "home";
 	}
 	
+	@RequestMapping("doA")
+	public void doA() {
+		logger.info("doA called..");
+	}
+	
+	@RequestMapping("doB")
+	public void doB() {
+		logger.info("doB called..");
+	}
+	
+	@RequestMapping("doC")
+	public String doC(@ModelAttribute("msg") String msg) {
+		logger.info("doC called..");
+		
+		return "result";
+	}
+	
+	@RequestMapping("/doD")
+	public String doD(Model model) {
+		//make sample data
+		ProductVO product = new ProductVO("Sample Product", 10000);
+		
+		logger.info("doD");
+		
+		model.addAttribute(product);
+		
+		return "productDetail";
+	}
+	
+	@RequestMapping("/doE")
+	public String doE(RedirectAttributes rttr) {
+		logger.info("doE -> doF");
+		rttr.addFlashAttribute("msg", "This is the Message!! with redirected");
+		return "redirect:/doF";
+	}
+	
+	@RequestMapping("doF")
+	public void doF(@ModelAttribute String msg) {
+		logger.info("doF called.."+msg);
+	}
+	
+	@RequestMapping("/doJSON")
+	public @ResponseBody ProductVO doJSON() {
+		ProductVO vo = new ProductVO("샘플상품", 30000);	
+		return vo;
+	}
 }
