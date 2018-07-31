4e668f53debe354143fafe2b3726fb9bb86951df
mark146 <tkdtkdqja@naver.com>
2018-07-31 17:30:45 +0900

    파일 업로드 이미지 파일 링크 처리 작업

--- exam_5/src/main/java/org/com/controller/UploadController.java
+++ exam_5/src/main/java/org/com/controller/UploadController.java
@@ -1,21 +1,18 @@
 package org.com.controller;
 
-import java.io.File;
+import java.io.*;
 import java.util.UUID;
-
 import javax.annotation.Resource;
 
-import org.com.util.UploadFileUtils;
+import org.apache.commons.io.IOUtils;
+import org.com.util.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
@@ -65,5 +62,40 @@ public class UploadController {
 	
 		return new ResponseEntity<>(UploadFileUtils.uploadFile(uploadPath, 
 				file.getOriginalFilename(), file.getBytes()), HttpStatus.CREATED);
+	}
+	
+	@ResponseBody
+	@RequestMapping("/displayFile")
+	public ResponseEntity<byte[]> displayFile(String fileName) throws Exception {
+		InputStream in = null;
+		ResponseEntity<byte[]> entity = null;
+		
+		logger.info("FILE NAME : "+fileName);
+		
+		try {
+			String formatName = fileName.substring(fileName.lastIndexOf(".") + 1);
+			MediaType mType = MediaUtils.getMediaType(formatName);
+			HttpHeaders  headers = new HttpHeaders();
+			in = new FileInputStream(uploadPath + fileName);
+			
+			if(mType != null) {
+				headers.setContentType(mType);
+			} else {
+				fileName = fileName.substring(fileName.indexOf("_") + 1);
+				headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+				headers.add("Content-Disposition", "attachment; filename=\"" + 
+				new String(fileName.getBytes("UTF-8"), "ISO-8859-1") + "\"");
+			}
+			
+			entity = new ResponseEntity<byte[]>(IOUtils.toByteArray(in),
+					headers,
+					HttpStatus.CREATED);
+		} catch(Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<byte[]>(HttpStatus.BAD_REQUEST);
+		} finally {
+			in.close();
+		}
+		return entity;
 	}
 }
--- exam_5/src/main/webapp/WEB-INF/views/uploadAjax.jsp
+++ exam_5/src/main/webapp/WEB-INF/views/uploadAjax.jsp
@@ -28,15 +28,12 @@ small {
 	 
   $(".fileDrop").on("drop", function(event) {
 	  event.preventDefault();
-	  
+  
 	  var files = event.originalEvent.dataTransfer.files;
 	  var file = files[0];
-	  
-	  //console.log(file);
 	  var formData = new FormData();
-	  
 	  formData.append("file", file);
-	  
+
 	  $.ajax({
 		 url: "/uploadAjax",
 		 data: formData,
@@ -45,10 +42,49 @@ small {
 		 contentType: false,
 		 type: "POST",
 		 success: function(data) {
-			 alert(data);
+			 var str = "";
+			 
+			 console.log(data);
+			 console.log(checkImageType(data));
+			 
+			 if(checkImageType(data)) {
+				 str = "<div>" +
+				 "<div><a href='displayFile?fileName="+ getImageLink(data) +"'>" +
+				 "<img src='displayFile?fileName=" + data + "'/>" 
+						 + getImageLink(data) + "</a></div>";
+			 } else {
+				 str = "<div><a href='displayFile?fileName="+data+"'>"
+				 + getOriginalName(data) +"</a></div>";
+			 }
+			 
+			$(".uploadedList").append(str); 
 		 }
 	  });
   });
+  
+  function checkImageType(fileName) {
+	  var pattern = /jpg|gif|png|jpeg/i;
+	  return fileName.match(pattern);
+  }
+  
+  function getOriginalName(fileName) {
+	  if(checkImageType(fileName)) {
+		  return;
+	  }
+	  var idx = fileName.indexOf("_") + 1;
+	  return fileName.substr(idx);
+  }
+  
+  function getImageLink(fileName) {
+	  if (!checkImageType(fileName)) {
+		  return;
+	  }
+	  var front = fileName.substr(0,12);
+	  var end = fileName.substr(14);
+	  
+	  return front + end;
+  }
+  
  });
  </script>
 <body>
