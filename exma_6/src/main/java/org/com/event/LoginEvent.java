3418ff28f6a38c23d6c378e593a564d7f2b44b51
mark146 <tkdtkdqja@naver.com>
2018-09-05 19:40:49 +0900

    파일 수정

--- exma_6/src/main/java/org/com/event/LoginEvent.java
+++
@@ -1,9 +0,0 @@
-package org.com.event;
-
-public class LoginEvent extends AuthenticationEvent
-{
-    public LoginEvent(String username)
-    {
-        super(username);
-    }
-}
--- exma_6/src/main/java/org/com/event/LogoutEvent.java
+++
@@ -1,9 +0,0 @@
-package org.com.event;
-
-public class LogoutEvent extends AuthenticationEvent
-{
-    public LogoutEvent(String username)
-    {
-        super(username);
-    }
-}
