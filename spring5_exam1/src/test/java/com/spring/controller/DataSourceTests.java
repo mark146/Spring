ef756328a41b7248dba6de1bc7c9367eb25575f7
mark146 <tkdtkdqja@naver.com>
2018-09-18 06:46:11 +0900

    예제 실습 및 예외처리 추가 작업

--- spring5_exam1/src/test/java/com/spring/controller/DataSourceTests.java
+++ spring5_exam1/src/test/java/com/spring/controller/DataSourceTests.java
@@ -1,13 +1,19 @@
 package com.spring.controller;
 
 import static org.junit.Assert.fail;
+
 import java.sql.Connection;
+
 import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
@@ -18,11 +24,17 @@ public class DataSourceTests {
 
 	@Setter(onMethod_ = { @Autowired })
 	private DataSource dataSource;
+
+	@Setter(onMethod_ = { @Autowired })
+	private SqlSessionFactory sqlSessionFactory;
+
 	
 	@Test
 	public void testConnection() {
-		try(Connection con = dataSource.getConnection()) {
+		try(SqlSession session = sqlSessionFactory.openSession();
+			Connection con = dataSource.getConnection();) {
 			
+			log.info(session);
 			log.info(con);
 			
 		} catch(Exception e) {
---
+++ spring5_exam1/src/test/java/com/spring/controller/TimeMapperTests.java
@@ -0,0 +1,35 @@
+package com.spring.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.spring.mapper.TimeMapper;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring/config/root-context.xml")
+@Log4j
+public class TimeMapperTests {
+	
+	@Setter(onMethod_ = @Autowired)
+	private TimeMapper timeMapper;
+	
+	@Test
+	public void testGetTime() {
+		log.info("getTime");
+		log.info(timeMapper.getClass().getName());
+		log.info(timeMapper.getTime());
+	}
+	
+	@Test
+	public void testGetTime2() {
+		log.info("getTime2");
+		log.info(timeMapper.getTime2());
+	}
+	
+}
